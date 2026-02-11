b2ad72e7d9127b56a0dc5e9f70587c812403b55f
andrew Peterson <andrew@andrews-Mini.localdomain>
2026-02-10 19:38:24 -0700

    Add Prisma db helper + tsconfig alias

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,7 +8,7 @@ declare global {
 export const prisma =
   global.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
